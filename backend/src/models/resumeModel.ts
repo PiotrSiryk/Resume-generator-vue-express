873d788f34d9ab086f871dd7fd6a590d02434cee
Psiryk <Piotr.Siryk@gmail.com>
2023-02-04 18:19:39 +0100

    import suggestions fixes

--- backend/src/models/resumeModel.ts
+++ backend/src/models/resumeModel.ts
@@ -1,10 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/client";
 
 export class Resume {
   id: string = "";
   name: string = "";
   async saveResumeToDb() {
-    const prisma = new PrismaClient();
-    await prisma.resume.create({ data: { name: this.name } });
+    // await prisma.resume.create({ data: { name: this.name } });
   }
 }
