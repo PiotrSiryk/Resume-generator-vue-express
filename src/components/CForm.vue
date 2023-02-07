b283ab904b9b8c3b67537973ea97bc833fe4a68f
Psiryk <Piotr.Siryk@gmail.com>
2023-02-07 23:02:57 +0100

    basic login form

---
+++ src/components/CForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-form ref="form">
+    <slot></slot>
+  </v-form>
+</template>
+
+<script setup lang="ts">
+defineExpose({ test: "ddd" });
+</script>
--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>Login</div>
+  <div>
+    <CForm ref="loginForm">
+      <v-text-field v-model="loginInfo.name" label="Login"></v-text-field>
+      <v-text-field
+        v-model="loginInfo.password"
+        label="Password"
+      ></v-text-field>
+      <v-btn>TESt</v-btn>
+    </CForm>
+  </div>
 </template>
 <script lang="ts" setup>
-import router from "@/router/router";
+import CForm from "@/components/CForm.vue";
+import { ref } from "vue";
+
+const loginForm = ref<InstanceType<typeof CForm>>();
+
+const loginInfo = {
+  name: "",
+  password: "",
+};
 </script>
 <style lang="scss"></style>
